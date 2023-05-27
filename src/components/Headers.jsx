88b3ccd95e0517e07c8f2cd58153c9e6d2660ecd
migueluisV <luise.miguelv@gmail.com>
2023-05-26 19:39:22 -0700

    Corrección de bug pantalla blanca por memoria

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import Logoescolar from '../Imagenes/Logoescolar.png';
-import Stack from 'react-bootstrap/Stack';
+import Logoescolar from "../Imagenes/Logoescolar.png";
+import Stack from "react-bootstrap/Stack";
 
-
-function Header(){
-    return(
-      <div className="App-header">
-
-        <Stack direction="horizontal" gap={3} className="App-he">
-      <div> <img src ={Logoescolar} width={70}
-        height={70}
-        alt="70x70"/></div>
-      <div>Round Robin con Prioridad y Retroalimentación</div>
-      <div className="ms-auto"></div>
-    </Stack>
-      </div>
-    );
+function Header() {
+  return (
+    <div className="App-header">
+      <Stack direction="horizontal" gap={3} className="App-he">
+        <div>
+          {" "}
+          <img src={Logoescolar} width={70} height={70} alt="70x70" />
+        </div>
+        <div>Round Robin con Prioridad y Retroalimentación</div>
+        <div className="ms-auto"></div>
+      </Stack>
+    </div>
+  );
 }
 export default Header;
